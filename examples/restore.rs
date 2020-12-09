d38c4339c06efea6b8b8e20f474bc37bb00b2d35
wngr <oliver@wngr.de>
2020-12-09 16:55:24 +0100

    Restore from sqlite file into an in memory database for testing
    Closes #5725

---
+++ examples/restore.rs
@@ -0,0 +1,10 @@
+use ipfs_sqlite_block_store::{BlockStore, Config};
+use tracing_subscriber::EnvFilter;
+
+fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+    let _ = BlockStore::open_test("test.sqlite", Config::default()).unwrap();
+    Ok(())
+}
