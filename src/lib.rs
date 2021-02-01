708701f976745340c55dc415b19dee9b3ba815bb
David Craven <david@craven.ch>
2021-02-01 15:58:37 +0100

    Use fnv.

--- src/lib.rs
+++ src/lib.rs
@@ -70,6 +70,7 @@ use crate::cidbytes::CidBytes;
 use cache::{BlockInfo, CacheTracker, NoopCacheTracker, WriteInfo};
 use db::*;
 pub use error::{BlockStoreError, Result};
+use fnv::FnvHashSet;
 use libipld::{
     cid::{self, Cid},
     codec::Codec,
@@ -79,7 +80,6 @@ use libipld::{
 use parking_lot::Mutex;
 use rusqlite::{Connection, DatabaseName};
 use std::{
-    collections::BTreeSet,
     convert::TryFrom,
     fmt,
     iter::FromIterator,
@@ -651,7 +651,7 @@ impl BlockStore {
                     let links = links(&block)?
                         .iter()
                         .map(CidBytes::try_from)
-                        .collect::<std::result::Result<BTreeSet<_>, cid::Error>>()?;
+                        .collect::<std::result::Result<FnvHashSet<_>, cid::Error>>()?;
                     let res = put_block(txn, &cid_bytes, &block.data(), links, &mut pin0)?;
                     Ok(WriteInfo::new(
                         BlockInfo::new(res.id, block.cid(), block.data().len()),
